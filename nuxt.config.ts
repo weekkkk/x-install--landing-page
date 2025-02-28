ec7d8c2ee35c73c0db71c83afd32f52c7f594b21
Nikita Nedelko <weekkkk@users.noreply.github.com>
2025-02-28 19:58:57 +0300

    init continue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,23 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
   modules: ["@nuxt/image", "@nuxt/ui"],
+  tailwindcss: {
+    cssPath: "~/src/app/assets/css/tailwind.css",
+  },
   ui: {
     global: true,
   },
   colorMode: {
     preference: "dark",
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "/fonts/sf-pro-display/sf-pro-display.style.css",
+        },
+      ],
+    },
   },
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 
 export default <Partial<Config>>{
-  // content: ["./**/*.{js,vue,ts}"],
   theme: {
     extend: {
       colors: {
@@ -10,6 +9,16 @@ export default <Partial<Config>>{
           0: "#95FFEA",
         },
         green: "#6AE15F",
+        violet: "#8706D6",
+      },
+      fontSize: {
+        base: ["1rem", "1.2"],
+        lg: ["1.25rem", "1.2"],
+        xl: ["1.5rem", "1.2"],
+        "2xl": ["2.5rem", "1.2"],
+        "3xl": ["3rem", "4rem"],
+        "4xl": ["3.5rem", "1.2"],
+        "5xl": ["4rem", "4.75rem"],
       },
     },
   },
