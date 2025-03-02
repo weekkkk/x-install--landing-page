a604d2fd2993d276c91deaabce99295c1c34b91b
Denis <denis.kosssa@gmail.com>
2025-03-02 03:57:01 +0300

    [QGW46jb2] Кастомизировать компоненты NuxtUI - кастомизировал кнопку

--- src/app/routes/index.vue
+++ src/app/routes/index.vue
@@ -1,3 +1,5 @@
 <template>
-  <UButton>test</UButton>
+  <UButton size="sm">Связаться</UButton>
+  <UButton size="md" disabled>Начать</UButton>
+  <UButton color="primary" variant="outline" size="mobile" disabled>Мобилка</UButton>
 </template>
