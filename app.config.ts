0465d86da1c400aa3c7d7f38b94b875c90fad3d8
Denis <denis.kosssa@gmail.com>
2025-03-02 15:57:45 +0300

    [QGW46jb2] Кастомизировать компоненты NuxtUI - кастомизировал переключатель

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,42 @@
+
 export default defineAppConfig({
   ui: {
+    strategy: 'override',
     button: {
       rounded: "rounded-full",
+      size: {
+        sm: 'text-[1rem]',
+        md: 'text-[1rem]',
+      },
+      padding: {
+        sm: 'py-[1rem] px-[2.5rem]',
+        md: 'py-[1rem] px-[3.25rem]',
+      },
       color: {
         primary: {
+          outline: "hover:dark:bg-white hover:dark:text-black border dark:disabled:bg-[#383838] dark:disabled:text-[#8A8A8A] dark:disabled:border-none",
           solid:
-            "shadow-sm dark:text-black dark:bg-gradient-to-r dark:from-turquoise-100 dark:to-green hover:dark:to-turquoise-0",
+            "shadow-sm dark:text-black dark:bg-gradient-to-r dark:from-turquoise-100 dark:to-green hover:dark:to-turquoise-0 dark:disabled:to-[#383838] dark:disabled:from-[#383838] dark:disabled:text-[#8A8A8A]",
         },
+
       },
     },
+    tabs: {
+      list: {
+        base: 'top-0, bottom-0, h-auto',
+        padding: "p-0",
+        rounded: 'rounded-[2.5rem]',
+        background: 'dark:bg-[#151515]',
+        marker: {
+          rounded: 'rounded-[2.5rem]',
+          background: 'dark:bg-gradient-to-r dark:from-turquoise-100 dark:to-green'
+        },
+        tab: {
+          active: 'dark:text-black',
+          inactive: 'dark:text-white',
+          size: 'text-[1.25rem]',
+        }
+      }
+    }
   },
 });
--- src/app/routes/index.vue
+++ src/app/routes/index.vue
@@ -1,5 +1,14 @@
 <template>
   <UButton size="sm">Связаться</UButton>
   <UButton size="md" disabled>Начать</UButton>
-  <UButton color="primary" variant="outline" size="mobile" disabled>Мобилка</UButton>
+  <UButton color="primary" variant="outline" size="md">Мобилка</UButton>
+  <UTabs :items="items" class="w-[10rem]" />
 </template>
+
+<script setup>
+const items = [{
+  label: 'EN',
+}, {
+  label: 'RU',
+}]
+</script>
