04d17f189c1f1a2890d50eaa13e6355e178024a6
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-03-05 23:19:19 +0200

    create scroll

--- src/pages/main/ui/page/index.vue
+++ src/pages/main/ui/page/index.vue
@@ -12,4 +12,13 @@ import { MainPageSectionsFeedBackSection } from "#components";
 useHead({
   title: "X-Install",
 });
+onMounted(() => {
+  document.addEventListener("scroll", () => {
+    document.querySelectorAll(".moving-element").forEach((el) => {
+      const speed = el.dataset.speed || 2; // Можно задать разную скорость через data-атрибут
+      const offset = window.scrollY / speed;
+      el.style.transform = `translateY(${-offset}px)`;
+    });
+  });
+});
 </script>
