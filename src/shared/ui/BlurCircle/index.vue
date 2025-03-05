216f17f5c95b348704bb7e22d36e8a44153f6de5
Denis <denis.kosssa@gmail.com>
2025-03-05 22:38:31 +0300

    [96r4FJFZ] - Страница приветствия - Правки

--- src/shared/ui/BlurCircle/index.vue
+++ src/shared/ui/BlurCircle/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import type { UiBlurCircleProps } from './types';
+import type { UiBlurCircleProps } from "./types";
 
-const props = defineProps<UiBlurCircleProps>()
-
+const props = defineProps<UiBlurCircleProps>();
 </script>
 <template>
-    <div class="w-[15.7rem] h-60 rounded-full blur-[15rem] -z-10" :class="[`${props.color}`]" />
+  <div
+    class="w-[15.7rem] h-60 rounded-full blur-[15rem] -z-10"
+    :class="`${props.color}`"
+  />
 </template>
