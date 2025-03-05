9f72dc1f72519fa9d93165cda9f99c1d47ec4e19
Denis <denis.kosssa@gmail.com>
2025-03-06 02:10:03 +0300

    [GEfdTyrb] - Телефон - Создать экран с телефоном

--- src/app/routes/index.vue
+++ src/app/routes/index.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="ml-10 mr-8 my-[2.35rem]">
-    <MainPage />
+  <div class="relative w-full overflow-hidden">
+    <div class="ml-10 mr-8 my-[2.35rem]">
+      <MainPage />
+    </div>
   </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
--- src/pages/main/ui/page/index.vue
+++ src/pages/main/ui/page/index.vue
@@ -1,6 +1,7 @@
 <template>
   <LayoutHeaderWidget />
   <MainPageSectionsWelcome class="mt-[10.65rem] h-[52.55rem]" />
+  <MainPageSectionsPhone class="overflow-hidden" />
 </template>
 
 <script setup>
