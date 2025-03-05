e4eaf4932811419980cd7d44f76449e3a3a939ac
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-03-05 23:19:19 +0200

    create scroll

--- src/pages/main/ui/page/index.vue
+++ src/pages/main/ui/page/index.vue
@@ -2,10 +2,20 @@
   <LayoutHeaderWidget />
   <MainPageSectionsWelcome class="mt-[10.65rem] h-[52.55rem]" />
   <MainPageSectionsPhone class="overflow-hidden" />
+  <div class="h-[1000px]"></div>
 </template>
 
 <script setup>
 useHead({
   title: "X-Install",
+});
+onMounted(() => {
+  document.addEventListener("scroll", () => {
+    document.querySelectorAll(".moving-element").forEach((el) => {
+      const speed = el.dataset.speed || 2; // Можно задать разную скорость через data-атрибут
+      const offset = window.scrollY / speed;
+      el.style.transform = `translateY(${-offset}px)`;
+    });
+  });
 });
 </script>
