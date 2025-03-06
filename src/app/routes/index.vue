a215ed1e18b9ff4934e5d2ba3d3e7c0d63102b9a
Denis <denis.kosssa@gmail.com>
2025-03-06 22:51:45 +0300

    [GEfdTyrb] - Телефон - Создать секцию с телефоном

--- src/app/routes/index.vue
+++ src/app/routes/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative w-full overflow-hidden">
-    <div class="ml-10 mr-8 my-[2.35rem]">
+    <div class="ml-10 mr-10 my-[2.35rem]">
       <MainPage />
     </div>
   </div>
