96458d369ee7bb260d000818cc07b61a7d564e94
Denis <denis.kosssa@gmail.com>
2025-03-09 17:43:17 +0300

    [96r4FJFZ] - Страница приветствия - Адаптив

--- src/pages/main/ui/page/index.vue
+++ src/pages/main/ui/page/index.vue
@@ -1,10 +1,12 @@
 <template>
-  <LayoutHeaderWidget />
-  <MainPageSectionsWelcome class="mt-[10.65rem] h-[52.55rem]" />
+  <!-- <LayoutHeaderWidget /> -->
+  <MainPageSectionsWelcome
+    class="mt-[10.65rem] max-md:mt-[7rem] h-[52.55rem]"
+  />
 </template>
 
 <script setup>
 useHead({
-  title: 'X-Install'
-})
+  title: "X-Install",
+});
 </script>
