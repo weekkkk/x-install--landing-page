29b61c86c37126c9b0ded7aa3f85a16a6075bd2a
AlexeyFrunza <tvoih8r@mail.ru>
2025-03-13 01:36:34 +0200

    [U6cYuTDF] Повысить качество фото в блоке Телефон -- все возможные фотографии были заменены на svg -- переделана верстка по pixel perfect

--- src/pages/main/ui/page/sections/phone/index.vue
+++ src/pages/main/ui/page/sections/phone/index.vue
@@ -1,45 +1,75 @@
 <template>
-  <div class="h-[46.3rem]">
+  <div class="h-[46.3rem] max-md:h-[37.5rem]">
     <NuxtImg
       src="/images/body.webp"
-      class="moving-element absolute max-md:left-[5.3545rem] max-md:w-[8.936rem] max-md:h-[18.301rem] w-[23.86rem] h-[48.7325rem] top-[58.65rem] left-[36.6rem] z-10"
+      class="moving-element absolute max-md:left-[5.3545rem] max-md:w-[8.936rem] max-md:h-[18.301rem] w-[23.86rem] h-[48.7325rem] top-[58.65rem] left-[36.055rem] z-10"
     />
     <NuxtImg
       src="/images/screen.webp"
-      class="moving-element max-md:left-[5.8rem] max-md:rounded-none max-md:top-[59rem] absolute top-[59.65rem] max-md:w-[8.05rem] max-md:h-[17.5rem] left-[37.96rem] w-[21.5rem] h-[46.6rem] rounded-tl-[2.5rem] rounded-tr-[2.5rem] rounded-bl-[3rem] rounded-br-[3rem]"
+      class="moving-element max-md:left-[5.8rem] max-md:rounded-none max-md:top-[59rem] absolute top-[59.65rem] max-md:w-[8.05rem] max-md:h-[17.5rem] left-[37.24rem] w-[21.5rem] h-[46.6rem] rounded-tl-[2.5rem] rounded-tr-[2.5rem] rounded-bl-[3rem] rounded-br-[3rem]"
     />
     <NuxtImg
-      src="/images/application1.webp"
-      class="moving-element w-[28rem] max-md:left-[4.608rem] max-md:top-[62.8rem] max-md:w-[10.331rem] max-md:h-[2.944rem] h-[8.25rem] absolute top-[69.3rem] left-[34.333rem] z-20"
+      src="/images/application1.png"
+      class="moving-element w-[26.75rem] max-md:left-[4.808rem] max-md:top-[63rem] max-md:w-[10.031rem] max-md:h-[2.344rem] h-[6.25rem] absolute top-[70.15rem] left-[34.6rem] z-20"
     />
     <NuxtImg
-      src="/images/chart1.webp"
-      class="moving-element max-md:left-[13.77rem] max-md:top-[59.9rem] max-md:w-[4.1rem] max-md:h-[2.138rem] w-[10.8455rem] h-[6.2rem] absolute top-[62.8rem] left-[59.2rem] z-20"
+      src="/images/chart1.svg"
+      class="moving-element max-md:left-[13.77rem] max-md:top-[59.6rem] max-md:w-[4.2rem] max-md:h-[2.35rem] w-[10.8455rem] h-[6.2rem] absolute top-[62.8rem] left-[58.63rem] z-20"
       data-speed="3"
     />
     <NuxtImg
-      src="/images/rating.webp"
-      class="moving-element max-md:left-[2.706rem] max-md:top-[66.05rem] max-md:w-[3.1rem] max-md:h-[2.5875rem] w-[7.897rem] h-[6.9rem] absolute top-[77.05rem] left-[29rem] z-30"
+      src="/images/rating.svg"
+      class="moving-element max-md:left-[2.706rem] max-md:top-[65.85rem] max-md:w-[3.1rem] max-md:h-[2.5875rem] w-[8.35rem] h-[7.3rem] absolute top-[77.05rem] left-[28.44rem] z-30"
     />
     <NuxtImg
-      src="/images/rating2.webp"
-      class="moving-element max-md:left-[16.036rem] max-md:top-[69.5rem] max-md:w-[3.592rem] max-md:h-[2.788rem] w-[9.897rem] h-[7.1rem] absolute top-[88.5rem] left-[62.4345rem]"
+      src="/images/rating2.svg"
+      class="moving-element max-md:left-[15.876rem] max-md:top-[69.3rem] max-md:w-[3.592rem] max-md:h-[2.788rem] w-[9.897rem] h-[7.45rem] absolute top-[88.5rem] left-[62rem]"
     />
     <NuxtImg
-      src="/images/chart2.webp"
-      class="moving-element max-md:left-[.85rem] max-md:top-[72.07rem] max-md:w-[3.5405rem] max-md:h-[2.6875rem] w-[9.622rem] h-[7.2rem] absolute top-[95.85rem] left-[26.6225rem]"
+      src="/images/chart.svg"
+      class="moving-element max-md:left-[.85rem] max-md:top-[71.97rem] max-md:w-[3.5405rem] max-md:h-[2.6875rem] w-[9.5rem] h-[7.5rem] absolute top-[95.85rem] left-[26.32rem]"
     />
     <NuxtImg
-      src="/images/application2.webp"
-      class="moving-element max-md:left-[5.3705rem] max-md:top-[66.35rem] max-md:w-[8.925rem] max-md:h-[2.1rem] w-[23.8rem] h-[5.6rem] absolute top-[79.15rem] left-[36.6rem] z-20"
+      src="/images/application2.png"
+      class="moving-element max-md:left-[5.3705rem] max-md:top-[66.35rem] max-md:w-[8.925rem] max-md:h-[2.1rem] w-[23.8rem] h-[5.6rem] absolute top-[79.15rem] left-[36.1rem] z-20"
     />
     <UiBlurCircle
       color="bg-turquoise-100"
-      class="moving-element max-md:left-[6.8rem] max-md:w-[5.8875rem] max-md:top-[64.65rem] max-md:h-[5.625rem] absolute top-[73.65rem] left-[40.96rem] -z-[5]"
+      class="max-md:left-[6.8rem] max-md:!w-[5.8875rem] max-md:top-[54.52rem] max-md:!h-[5.625rem] max-md:!blur-[6rem] absolute top-[73.65rem] left-[40.14rem] -z-[5] !blur-[11rem] !w-[15.7rem] !h-[15rem]"
+    />
+    <UiBlurCircle
+      color="bg-turquoise-100"
+      class="absolute w-[12.85rem] h-[12.85rem] top-[39.75rem] right-[9.8rem]"
+    />
+    <UiGraph1 />
+    <NuxtImg
+      src="/images/noisegraph1.webp"
+      class="absolute max-md:w-[14.57rem] max-md:h-[9.49rem] max-md:top-[63.25rem] max-md:left-[0.1rem] w-[72.15rem] h-[46.3rem] left-[3.5rem] top-[66.05rem] -z-[1] mix-blend-overlay opacity-20"
     />
     <NuxtImg
-      src="/images/graph.webp"
-      class="w-full max-md:top-[55.2rem] max-md:left-[-0.2rem] absolute left-[.55rem] top-[66.05rem] -z-10"
+      src="/images/graph2.svg"
+      class="w-[36.45rem] h-[20.76rem] max-md:w-[7.26rem] max-md:h-[4.30rem] top-[76.32rem] max-md:top-[54.83rem] left-0 -z-[2] absolute"
+    />
+    <NuxtImg
+      src="/images/noisegraph2.webp"
+      class="w-[36.45rem] h-[20.76rem] max-md:w-[7.26rem] max-md:h-[4.30rem] max-md:top-[54.83rem] top-[76.32rem] left-0 -z-[2] absolute mix-blend-overlay -rotate-180 opacity-20"
+    />
+    <NuxtImg
+      src="/images/graph3.svg"
+      class="absolute max-md:w-[12.75rem] max-md:h-[7.02rem] max-md:top-[53.6rem] w-[61.3rem] h-[31.55rem] top-[68rem] right-0 -z-[2]"
+    />
+    <NuxtImg
+      src="/images/noisegraph3.webp"
+      class="absolute max-md:w-[14.9rem] max-md:h-[7.02rem] max-md:top-[53.6rem] w-[71.85rem] h-[29.05rem] top-[70.4rem] right-[1.4rem] -z-[1]"
+    />
+    <!-- <NuxtImg
+      src="/images/graph4.svg"
+      class=""
+    /> -->
+    <UiGraph4 />
+    <NuxtImg
+      src="/images/noisegraph4.webp"
+      class="absolute w-[72.04rem] h-[21rem] top-[85.85rem] right-0 -z-[1]"
     />
   </div>
 </template>
