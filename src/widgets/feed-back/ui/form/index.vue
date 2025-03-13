9c7464d76bb40697b3b12d8942eb44881f6890b6
AlexeyFrunza <tvoih8r@mail.ru>
2025-03-14 00:43:42 +0200

    [2DV8j79e] Поправить элипсы и сетку на мобилке - Мы поможем -- адаптировал сетку и элипс под мобилку

--- src/widgets/feed-back/ui/form/index.vue
+++ src/widgets/feed-back/ui/form/index.vue
@@ -30,25 +30,25 @@ const sendFormInfo = async () => {
 
 <template>
   <div class="flex flex-col items-center">
-    <div class="relative max-md:w-[17.65rem] text-center">
+    <div class="relative max-md:w-[19.65rem] text-center">
       <h1
         class="text-6xl max-md:text-[1.75rem] max-md:leading-[1.2] font-bold bg-gradient-to-r dark:from-turquoise-100 dark:to-green bg-clip-text text-transparent"
       >
         {{ $t("feedBackPage.help") }}
       </h1>
       <p
-        class="text-mediumGray text-center max-md:text-base text-xl mt-6 max-md:mt-4"
+        class="text-mediumGray text-center max-md:text-base text-xl mt-6 max-md:mt-4 max-md:pr-4 max-md:pl-4"
       >
         {{ $t("feedBackPage.consultation") }} <br class="max-md:hidden" />
         {{ $t("feedBackPage.proposal") }}
       </p>
       <UiBlurCircle
         color="bg-turquoise-100"
-        class="absolute w-[11.15rem] h-[11.15rem] max-md:w-[10.2rem] max-md:h-[10.2rem] max-md:left-16 left-24 bottom-4 -z-20"
+        class="absolute w-[11.15rem] h-[11.15rem] max-md:w-[10.2rem] max-md:h-[10.2rem] max-md:left-[4.75rem] max-md:-top-[2.95rem] left-24 bottom-4 -z-20 max-md:!blur-[10.5rem]"
       />
       <NuxtImg
         src="/images/grid.webp"
-        class="absolute w-[28rem] max-md:top-[-5rem] max-md:w-[19.65rem] max-md:h-[19.65rem] h-[28rem] top-[-1rem]"
+        class="absolute w-[28rem] max-md:-top-[7.55rem] max-md:!w-[19.65rem] max-md:!h-[19.65rem] h-[28rem] -top-[7.75rem] -z-30"
       />
     </div>
     <UForm
@@ -93,7 +93,7 @@ const sendFormInfo = async () => {
         type="submit"
         class="w-[45rem] max-md:text-xs max-md:w-[17.65rem] h-[6.75rem] max-md:h-[2.9rem] flex items-center justify-center text-lg leading-none mt-6 max-md:mt-2"
       >
-        {{ $t('feedBackPage.send') }}
+        {{ $t("feedBackPage.send") }}
       </UButton>
     </UForm>
   </div>
